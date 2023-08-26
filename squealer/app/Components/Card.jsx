26d1c940f22fed5731031cc787f1a335f05120a5
ariarru <arianna.arruzzoli@studio.unibo.it>
2023-08-26 17:10:38 +0200

    update logic bug

--- squealer/app/Components/Card.jsx
+++ squealer/app/Components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card ({ children, noPadding, isNavbar}) {
-  let classes = 'bg-white shadow-md shadow-gray-300 rounded-md mb-5 p-4';
-  if(noPadding){
-      classes += ' p-0';
+  let classes = 'bg-white shadow-md shadow-gray-300 rounded-md mb-5 p-0';
+  if(!noPadding){
+      classes += ' p-4';
   }
   if(isNavbar){
     classes += ' fixed';
