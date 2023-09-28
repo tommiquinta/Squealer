c1ee6f4254e826f7cf96cd9db34862aed9df5f6b
ariarru <arianna.arruzzoli@studio.unibo.it>
2023-09-28 16:03:30 +0200

    Update counter when reacting

--- squealer/app/components/reaction/Reaction.jsx
+++ squealer/app/components/reaction/Reaction.jsx
@@ -3,118 +3,63 @@ import LikeButton from './LikeButton.jsx';
 import DislikeButton from './DisLikeButton.jsx';
 import { useEffect, useState } from 'react';
 
-function Reaction({ numLikes, numDislikes, hasLiked, hasDisliked, like, dislike, disable }) {
-    //per ora non quadra
+function Reaction({ numLikes, numDislikes, hasLiked, hasDisliked, disable, addLike, addDislike, removeLike, removeDislike }) {
+
+    //aggiungi e rimuovi like/dislike deve stare in un server component e passare la function alla reaction,
+    //posso metterlo nel post ma non so se convenga
+    //va passato nella home fino alla reaction, ma devo fare una catena quindi devo vedere se funziona
     const [isLiked, setIsLiked] = useState(hasLiked ? true : false)
     const [isDisliked, setIsDisliked] = useState(hasDisliked ? true : false)
+    const [counterLikes, setCounterLikes] = useState(numLikes);
+    const [counterDislikes, setCounterDislikes] = useState(numDislikes);
 
-    function handleDislike(isDiliked){
-        if(isDiliked){
+    function handleDislike(toDislike){
+        if(toDislike){
             console.log("qui gestisci che deve aggiungere un dislike");
             //aggiungi dislike
             setIsDisliked(true);
+            setCounterDislikes(counterDislikes => counterDislikes+1 );
             handleLike(false);
         } else {
             console.log("qui gestisci il fatto che ha tolto il dislike");
             //rimuovi dislike
+            if(isDisliked){
+                setCounterDislikes(counterDislikes => counterDislikes-1 );
+            }
             setIsDisliked(false);
-            console.log("dislike: "+isDiliked);
         }
     }
 
-    function handleLike(isLiked){
-        if(isLiked){
+    function handleLike(toLike){
+        if(toLike){
             console.log("qui gestisci che deve aggiungere un like");
             //aggiungi like
             setIsLiked(true)
+            setCounterLikes(counterLikes => counterLikes+1);
             handleDislike(false)
         } else {
            console.log("qui gestisci il fatto che ha tolto il like");
             //rimuovi like
+            if(isLiked){
+                setCounterLikes(counterLikes => counterLikes-1);
+            }
             setIsLiked(false);
-            console.log("like: "+isLiked);
         }
     }
-
-
-    //tutta sta roba va passata nel componete che lo richiama, tramite like e dislike -> qui no supabase
-   /* async function addLike() {
-        try {
-            await supabase
-                .from('likes')
-                .upsert([
-                    {
-                        post_id: postId,
-                        user_id: userId
-                    }])
-                .then(() => {
-                    setLikeSelected(!likeSelected);
-                })
-        } catch (error) {
-            console.log(error, 'Errore nel like');
-        }
-    }
-
-    async function addDislike() {
-        try {
-            await supabase
-                .from('dislikes')
-                .upsert([
-                    {
-                        post_id: postId,
-                        user_id: userId
-                    }])
-                .then(() => {
-                    setDislikeSelected(!dislikeSelected);
-                })
-        } catch (error) {
-            console.log(error, 'Errore nel like');
-        }
-    } 
-
-    async function removeLike() {
-        try {
-            await supabase
-                .from('likes')
-                .delete()
-                .eq('post_id', postId)
-                .eq('user_id', userId)
-                .then(() => {
-                    setLikeSelected(!likeSelected);
-                })
-        } catch (error) {
-            console.log(error, 'Errore nel like');
-        }
-    }
-
-    async function removeDislike() {
-        try {
-            await supabase
-                .from('dislikes')
-                .delete()
-                .eq('post_id', postId)
-                .eq('user_id', userId)
-                .then(() => {
-                    setDislikeSelected(!dislikeSelected);
-                })
-        } catch (error) {
-            console.log(error, 'Errore nel like');
-        }
-    } */
 
     return (
         <div className='w-full h-[30px] gap-2 flex items-center'>
             <LikeButton
                 hasLiked={isLiked}
                 handleLikes={handleLike}
-                count = {numLikes} 
+                count = {counterLikes} 
                 toDisable={disable}
                />
             <DislikeButton
                 key={isDisliked}
                 hasDisliked={isDisliked}
                 handleDislike={handleDislike} 
-                count={numDislikes}
+                count={counterDislikes}
                 toDisable={disable}/>
             {/* inserisci views */}
         </div>
